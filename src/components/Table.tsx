cb41373c5e3aa6b1b954f00e875964711064622c
Joel Rubin <joelsrubin@joelrubxqw3vh0k.lan>
2022-07-26 12:28:12 -0500

    update border

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { Typography } from "../dizzy/Typography";
 import { marked } from "marked";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
 export type ToDo = {
@@ -19,9 +18,8 @@ function EmptyRow() {
   return (
     <li className="flex flex-row">
       <div className="px-10 py-5 text-center">Add A Todo</div>
-
       <div className="px-10 py-5 text-center">
-        <span className="text-green-500">🫥</span>
+        <span>🫥</span>
       </div>
     </li>
   );
@@ -34,31 +32,27 @@ export const Table: React.FC<TTableProps> = ({
 }) => {
   const [listRef] = useAutoAnimate<HTMLUListElement>();
   return (
-    <div>
-      <ul className="text-lg mx-auto w-full" ref={listRef}>
+    <div className="w-screen">
+      <ul className="text-lg mx-auto w-1/4 border rounded-md" ref={listRef}>
         {todos.length ? (
           todos
             .sort((a, b) => +a.completed - +b.completed)
             .map((todo) => (
               <li
                 key={todo.id}
-                className="flex flex-row w-full justify-between"
+                className="flex flex-row w-full justify-between odd:bg-slate-200"
               >
                 <div
-                  className={`cursor-pointer ${
-                    todo.completed && "line-through decoration-4 text-gray-200"
+                  className={`cursor-pointer p-4 ${
+                    todo.completed && "line-through decoration-4 text-black"
                   } transition-colors duration-500 `}
                   onClick={() => handleDone(todo)}
-                >
-                  <div
-                    className="pr-20 py-2"
-                    dangerouslySetInnerHTML={{
-                      __html: marked.parse(todo.text),
-                    }}
-                  ></div>
-                </div>
+                  dangerouslySetInnerHTML={{
+                    __html: marked.parse(todo.text),
+                  }}
+                />
 
-                <div className="py-2">
+                <div className="p-4">
                   <button
                     className="cursor-pointer"
                     onClick={() => deleteTodo(todo)}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useEffect, useState } from "react";
 import Input from "../components/Input";
 import { Table, ToDo } from "../components/Table";
 import { GlobalStyles } from "../dizzy/GlobalStyles";
